d4ae67aac160e7f49d2b49557245e2c339ee11e5
Paulo Júnio <paulo.nascimento@gruposfa.com.br>
2022-03-02 16:09:05 -0300

    atualizando o projeto

--- data-binding/src/app/output-property/output-property.component.ts
+++ data-binding/src/app/output-property/output-property.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, EventEmitter, Output} from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, ViewChild, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'contador',
@@ -11,12 +11,16 @@ export class OutputPropertyComponent implements OnInit {
 
   @Output() mudouValor = new EventEmitter();
 
+  @ViewChild('campoInput') campoValorInput: ElementRef | any;
+
   incrementa(){
-    this.valor++;
+    //direto da variavel do html component
+    this.campoValorInput.nativeElement.value++;
     this.mudouValor.emit({novoValor: this.valor})
   }
 
   decrementa(){
+    //pegando pela variavel local
     this.valor--;
     this.mudouValor.emit({novoValor: this.valor})
   }
